b875c11a2aefa085077502053eedc78fc8f4ecc5
Dominic Shillingford <dshillingford77@gmail.com>
2024-09-28 20:14:33 -0400

    Started the debts page.

--- app/[ledger]/debts/page.jsx
+++ app/[ledger]/debts/page.jsx
@@ -5,24 +5,34 @@
 import React, { useContext } from 'react';
 import { LedgerDataContext } from '../ledger-layout';
 
-const DebtsPage = () => {
+function DebtsRow(props) {
+  const {payer, payee, amount} = props;
+    
+  return (
+    <div className="flex flex-row justify-between my-3 rounded-lg bg-card px-4 py-3">
+      <div>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {payer} → {payee}
+          <p className="mt-1 text-sm font-normal tracking-tight text-gray-700 dark:text-gray-400">
+            ${amount}
+          </p>
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default function DebtsPage() {
     const { ledgerData } = useContext(LedgerDataContext);
     const { debts } = ledgerData;
 
+    const DebtsRows = () => debts.map(({payer, payee, amount}) => (
+      <DebtsRow key={`${payer}:${payee}`} payer={payer} payee={payee} amount={amount} />
+    ));
+
     return (
-        <div>
-            <h2>Debts</h2>
-            <hr className="py-2"/>
-            {debts.map(({payer, payee, amount}) => (
-                <div key={payer + ':' + payee} className="card">
-                    <p>
-                        {payer} owes {payee}: {amount}
-                    </p>
-                    <hr/>
-                </div>
-            ))}
-        </div>
+      <div className="mt-[70px]">
+        <DebtsRows />
+      </div>
     );
 };
-
-export default DebtsPage;
