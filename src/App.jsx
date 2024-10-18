1a9638ba8c956679972fdab6c654fdfc4b15f0e6
Tom Ginsberg <tom@beit.tech>
2024-10-18 13:11:32 -0400

    implemented lazy loading and child routes with layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 // App.jsx
-import React, { Suspense, useEffect, useMemo, useState } from "react"
-import { BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom"
+import React, { Suspense } from "react"
+import { BrowserRouter as Router, Outlet, Route, Routes, useParams } from "react-router-dom"
 import Toolbar from "@/components/toolbar"
 import Topbar from "@/components/topbar"
 import { ThemeProvider } from "@/components/theme-provider"
-import { generateRandomLedgerData } from "@/api/client.js"
+import { emptyExpense } from "@/api/client.js"
 import ExpenseDrawer from "@/components/expense-drawer"
 import HoldToDelete from "@/components/delete"
-import Fuse from "fuse.js"
 import useExpense from "@/hooks/use-expense.js"
 import { Toaster } from "@/components/ui/toaster"
 
@@ -19,119 +18,83 @@ const ExpensesTab = React.lazy(() => import("@/pages/expenses"))
 const MembersTab = React.lazy(() => import("@/pages/members"))
 const DebtsTab = React.lazy(() => import("@/pages/debts"))
 
-export default function App() {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:ledgerName">
-            <Route
-              index
-              element={
-                <Suspense fallback={<Loading />}>
-                  <LedgerApp target="expenses" />
-                </Suspense>
-              }
-            />
-            <Route
-              path="members"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <LedgerApp target="members" />
-                </Suspense>
-              }
-            />
-            <Route
-              path="debts"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <LedgerApp target="debts" />
-                </Suspense>
-              }
-            />
-          </Route>
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
-      <Toaster />
-    </ThemeProvider>
-  )
-}
-
-function LedgerApp({ target }) {
+function LedgerLayout() {
   const { ledgerName } = useParams()
-  const [searchTerm, setSearchTerm] = useState("")
-  const [filteredExpenses, setFilteredExpenses] = useState([])
+  const [searchTerm, setSearchTerm] = React.useState("")
 
-  const {
-    isDrawerOpen,
-    isEditMode,
-    selectedExpense,
-    openAddExpenseDrawer,
-    openEditExpenseDrawer,
-    closeExpenseDrawer,
-    isDeleteDrawerOpen,
-    closeDeleteDrawer,
-    onDeleteClick,
-    handleDelete,
-    copyExpense,
+  // State for ExpenseDrawer
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false)
+  const [isEditMode, setIsEditMode] = React.useState(false)
+  const [selectedExpense, setSelectedExpense] = React.useState(emptyExpense)
+
+  // Expense data and functions
+  const { expenses, members, isDeleteDrawerOpen, closeDeleteDrawer, onDeleteClick, handleDelete, copyExpense } =
+    React.useMemo(() => useExpense(ledgerName), [ledgerName])
+  const memberNames = members.map((member) => member.name)
+
+  // Functions to control ExpenseDrawer
+  const openAddExpenseDrawer = React.useCallback(() => {
+    setIsDrawerOpen(true)
+    setIsEditMode(false)
+    setSelectedExpense(emptyExpense)
+  }, [])
+
+  const openEditExpenseDrawer = React.useCallback((expense) => {
+    setIsDrawerOpen(true)
+    setIsEditMode(true)
+    setSelectedExpense(expense)
+  }, [])
+
+  const closeExpenseDrawer = React.useCallback(() => {
+    setIsDrawerOpen(false)
+  }, [])
+
+  // Context value to pass to child components
+  const outletContext = {
+    searchTerm,
     expenses,
-    members
-  } = useExpense(ledgerName)
-
-  const fuse = useMemo(
-    () =>
-      new Fuse(expenses, {
-        keys: ["name", "category"],
-        threshold: 0.1
-      }),
-    [expenses]
-  )
-
-  useEffect(() => {
-    if (searchTerm) {
-      const results = fuse.search(searchTerm)
-      setFilteredExpenses(results.map((result) => result.item))
-    } else {
-      setFilteredExpenses(expenses)
-    }
-  }, [searchTerm, fuse, expenses])
-
-  const CurrentTab = () => {
-    switch (target) {
-      case "expenses":
-        return (
-          <ExpensesTab
-            expenses={filteredExpenses}
-            openEditDrawer={openEditExpenseDrawer}
-            onDeleteClick={onDeleteClick}
-            onCopyClick={copyExpense}
-          />
-        )
-      case "members":
-        return <MembersTab ledgerName={ledgerName} />
-      case "debts":
-        return <DebtsTab ledgerName={ledgerName} />
-      default:
-        return <Error />
-    }
+    members,
+    openEditExpenseDrawer,
+    onDeleteClick,
+    copyExpense,
+    ledgerName
   }
 
   return (
     <>
-      <Topbar onSearch={setSearchTerm} pageType={target} />
-      <CurrentTab />
+      <Topbar onSearch={setSearchTerm} />
+      <Suspense fallback={<Loading />}>
+        <Outlet context={outletContext} />
+      </Suspense>
       <Toolbar ledger={ledgerName} onClickPlus={openAddExpenseDrawer} />
       <ExpenseDrawer
         selectedExpense={selectedExpense}
         isDrawerOpen={isDrawerOpen}
         isEditMode={isEditMode}
         handleCloseDrawer={closeExpenseDrawer}
-        members={members.map((member) => member.name)}
+        members={memberNames}
         onDeleteClick={onDeleteClick}
       />
       <HoldToDelete onConfirm={handleDelete} isDrawerOpen={isDeleteDrawerOpen} handleCloseDrawer={closeDeleteDrawer} />
     </>
+  )
+}
+
+export default function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:ledgerName" element={<LedgerLayout />}>
+            <Route index element={<ExpensesTab />} />
+            <Route path="members" element={<MembersTab />} />
+            <Route path="debts" element={<DebtsTab />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+      <Toaster />
+    </ThemeProvider>
   )
 }
