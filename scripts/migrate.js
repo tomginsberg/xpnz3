669c0af845472ed618bbb0239a2b4a88214c598e
Tom Ginsberg <tom@beit.tech>
2025-03-04 13:23:45 -0500

    change db path to data.db.local

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -1,191 +1,207 @@
-import Knex from 'knex';
-import fs from 'fs/promises';
-import path from 'path';
-import _ from 'lodash';
+import Knex from "knex"
+import fs from "fs/promises"
+import path from "path"
 
-const parse = JSON.parse;
+const parse = JSON.parse
 
-const db = Knex ({ client: 'sqlite3', connection: { filename: 'data.db' }, useNullAsDefault: true });
+const db = Knex({ client: "sqlite3", connection: { filename: "data.db.local" }, useNullAsDefault: true })
 
-import { formatLedgerName, getDateString, getDateTimeString, generateId, supportedCurrencies } from '../src/api/utilities.js';
+import {
+  formatLedgerName,
+  getDateString,
+  getDateTimeString,
+  generateId,
+  supportedCurrencies
+} from "../src/api/utilities.js"
 
-async function makeLedgersTable () {
-  await db.schema.raw ("CREATE TABLE `ledgers` (`name` varchar(255) collate nocase, primary key (`name`))")
-  await db.schema.table ('ledgers', table => { 
-    table.enu('currency', supportedCurrencies);
-    table.boolean('is_private').defaultTo(false);
-  });
+async function makeLedgersTable() {
+  await db.schema.raw("CREATE TABLE `ledgers` (`name` varchar(255) collate nocase, primary key (`name`))")
+  await db.schema.table("ledgers", (table) => {
+    table.enu("currency", supportedCurrencies)
+    table.boolean("is_private").defaultTo(false)
+  })
 }
 
-async function makeTransactionsTable () {
-  await db.schema.createTable ('transactions', table => {
-    table.string ('id').primary ();
-    table.string ('name');
-    table.enu ('currency', supportedCurrencies);
-    table.string ('category');
-    table.string ('expense_type');
-    table.datetime ('created_at');
-    table.date ('date');
-    table.float ('exchange_rate');
-    table.boolean ('is_template');
-    table.boolean ('is_deleted');
-  });
+async function makeTransactionsTable() {
+  await db.schema.createTable("transactions", (table) => {
+    table.string("id").primary()
+    table.string("name")
+    table.enu("currency", supportedCurrencies)
+    table.string("category")
+    table.string("expense_type")
+    table.datetime("created_at")
+    table.date("date")
+    table.float("exchange_rate")
+    table.boolean("is_template")
+    table.boolean("is_deleted")
+  })
 
-  await db.schema.raw ("ALTER TABLE `transactions` ADD COLUMN `ledger` varchar(255) collate nocase REFERENCES `ledgers` (`name`)");
+  await db.schema.raw(
+    "ALTER TABLE `transactions` ADD COLUMN `ledger` varchar(255) collate nocase REFERENCES `ledgers` (`name`)"
+  )
 }
 
-async function makeRecurrencesTable () {
-  await db.schema.createTable ('recurrences', table => {
-    table.string ('id').primary ();
-    table.string ('rrule');
-    table.string ('template_id').references ('id').inTable ('transactions');
-    table.string ('last_created_id').references ('id').inTable ('transactions'); // for idempotency
-  });
+async function makeRecurrencesTable() {
+  await db.schema.createTable("recurrences", (table) => {
+    table.string("id").primary()
+    table.string("rrule")
+    table.string("template_id").references("id").inTable("transactions")
+    table.string("last_created_id").references("id").inTable("transactions") // for idempotency
+  })
 }
 
-async function makeMembersTable () {
-  await db.schema.createTable ('members', table => {
-    table.string ('id').primary ();
-    table.boolean ('is_active');
-  });
+async function makeMembersTable() {
+  await db.schema.createTable("members", (table) => {
+    table.string("id").primary()
+    table.boolean("is_active")
+  })
 
   // add name column to members table with collation nocase using a raw query
-  await db.schema.raw ("ALTER TABLE `members` ADD COLUMN `name` varchar(255) collate nocase");
-  await db.schema.raw ("ALTER TABLE `members` ADD COLUMN `ledger` varchar(255) collate nocase REFERENCES `ledgers` (`name`)");
+  await db.schema.raw("ALTER TABLE `members` ADD COLUMN `name` varchar(255) collate nocase")
+  await db.schema.raw(
+    "ALTER TABLE `members` ADD COLUMN `ledger` varchar(255) collate nocase REFERENCES `ledgers` (`name`)"
+  )
 
   // add unique constraint to members table that enforces unique name per ledger
-  await db.schema.table ('members', table => {
-    table.unique (['name', 'ledger']);
-  });
+  await db.schema.table("members", (table) => {
+    table.unique(["name", "ledger"])
+  })
 }
 
-function makeTransactionsMembersJunction (table) {
-  table.string ('transaction_id').references ('id').inTable ('transactions');
-  table.string ('member_id').references ('id').inTable ('members');
-  table.integer ('amount'); // integer in cents
-  table.float ('weight');
-  table.primary (['transaction_id', 'member_id']);
+function makeTransactionsMembersJunction(table) {
+  table.string("transaction_id").references("id").inTable("transactions")
+  table.string("member_id").references("id").inTable("members")
+  table.integer("amount") // integer in cents
+  table.float("weight")
+  table.primary(["transaction_id", "member_id"])
 }
 
-async function createTables () {
-  await makeLedgersTable ();
-  await makeTransactionsTable ();
-  await makeMembersTable ();
-  await db.schema.createTable ('transactions_member_junction', makeTransactionsMembersJunction);
-  await makeRecurrencesTable ();
+async function createTables() {
+  await makeLedgersTable()
+  await makeTransactionsTable()
+  await makeMembersTable()
+  await db.schema.createTable("transactions_member_junction", makeTransactionsMembersJunction)
+  await makeRecurrencesTable()
 }
 
-function reduceToObject (array, keyFunction, valueFunction) {
-  return array.reduce ((accumulator, current) => {
-    accumulator[keyFunction (current)] = valueFunction (current);
-    return accumulator;
-  }, {});
+function reduceToObject(array, keyFunction, valueFunction) {
+  return array.reduce((accumulator, current) => {
+    accumulator[keyFunction(current)] = valueFunction(current)
+    return accumulator
+  }, {})
 }
 
-async function importTransactionsFromJsonFile (filename, ledgername) {
-  await db ('ledgers').insert ({ name: ledgername, currency: 'CAD' });
+async function importTransactionsFromJsonFile(filename, ledgername) {
+  await db("ledgers").insert({ name: ledgername, currency: "CAD" })
 
-  var json = await fs.readFile (filename);
-  var txs = parse (json);
+  var json = await fs.readFile(filename)
+  var txs = parse(json)
 
-  console.log (txs)
+  console.log(txs)
 
   // return if txs is empty
-  if (!txs) return;
+  if (!txs) return
 
   // if txs is just an object, put it in an array
-  if (!Array.isArray (txs)) txs = [txs];
+  if (!Array.isArray(txs)) txs = [txs]
 
   // strip whitespace from fields
   for (var tx of txs) {
-    if (tx.name) tx.name = tx.name.trim ();
-    if (tx.category) tx.category = tx.category.trim ();
+    if (tx.name) tx.name = tx.name.trim()
+    if (tx.category) tx.category = tx.category.trim()
 
-    tx.by.members = tx.by.members.map (member => member.trim ());
-    tx.for.members = tx.for.members.map (member => member.trim ());
+    tx.by.members = tx.by.members.map((member) => member.trim())
+    tx.for.members = tx.for.members.map((member) => member.trim())
   }
-  
 
   // get all unique members in the transactions
-  var members = [...new Set (txs.map (tx => [...tx.for.members, ...tx.by.members]).flat ())];
-  var membersAndIds = members.map (name => ({ id: generateId (), name, ledger: ledgername, is_active: true }));
+  var members = [...new Set(txs.map((tx) => [...tx.for.members, ...tx.by.members]).flat())]
+  var membersAndIds = members.map((name) => ({ id: generateId(), name, ledger: ledgername, is_active: true }))
 
-  await db ('members').insert (membersAndIds);
+  await db("members").insert(membersAndIds)
 
-  var txsMemberJunctionRows = [];
+  var txsMemberJunctionRows = []
 
   // build the rows for the transactions_member_junction table (but use a two-dimensional array so we can add tx_id later)
   for (var tx of txs) {
-    var transactionMemberPairs = reduceToObject (membersAndIds, ({name}) => name, ({id}) => ({ member_id: id, amount: 0, weight: 0 }));
+    var transactionMemberPairs = reduceToObject(
+      membersAndIds,
+      ({ name }) => name,
+      ({ id }) => ({ member_id: id, amount: 0, weight: 0 })
+    )
 
-    tx.for.members.forEach ((name, index) => transactionMemberPairs[name].weight = tx.for.split_weights[index]);
-    tx.by.members.forEach ((name, index) => transactionMemberPairs[name].amount = Math.floor (Math.abs(tx.by.split_values[index]) * 100)); // convert to cents
+    tx.for.members.forEach((name, index) => (transactionMemberPairs[name].weight = tx.for.split_weights[index]))
+    tx.by.members.forEach(
+      (name, index) => (transactionMemberPairs[name].amount = Math.floor(Math.abs(tx.by.split_values[index]) * 100))
+    ) // convert to cents
 
-    transactionMemberPairs = Object.values (transactionMemberPairs);
+    transactionMemberPairs = Object.values(transactionMemberPairs)
 
     // remove members with zero amount and weight
-    transactionMemberPairs = transactionMemberPairs.filter (pair => pair.amount !== 0 || pair.weight !== 0);
+    transactionMemberPairs = transactionMemberPairs.filter((pair) => pair.amount !== 0 || pair.weight !== 0)
 
     // flip negative weights
-    transactionMemberPairs = transactionMemberPairs.map (pair => ({ ...pair, weight: pair.weight < 0 ? -pair.weight : pair.weight }));
+    transactionMemberPairs = transactionMemberPairs.map((pair) => ({
+      ...pair,
+      weight: pair.weight < 0 ? -pair.weight : pair.weight
+    }))
 
-    txsMemberJunctionRows.push (Object.values (transactionMemberPairs));
+    txsMemberJunctionRows.push(Object.values(transactionMemberPairs))
   }
 
   // clean up the rows for the transactions table
   for (var tx of txs) {
     // change date format from epoch to ISO
-    tx.created_at = getDateTimeString (new Date (tx.date));
-    tx.date = getDateString (new Date (tx.date));
+    tx.created_at = getDateTimeString(new Date(tx.date))
+    tx.date = getDateString(new Date(tx.date))
 
-    tx.ledger = ledgername;
-    tx.is_template = false;
-    tx.is_deleted = false;
+    tx.ledger = ledgername
+    tx.is_template = false
+    tx.is_deleted = false
 
-    if (tx.name === undefined || tx.name === null) tx.name = '';
-    if (tx.category === undefined || tx.category === null) tx.category = '';
+    if (tx.name === undefined || tx.name === null) tx.name = ""
+    if (tx.category === undefined || tx.category === null) tx.category = ""
 
-    if (tx.name === '' && tx.category === '') {
-      tx.name = 'Unnamed transaction';
+    if (tx.name === "" && tx.category === "") {
+      tx.name = "Unnamed transaction"
     }
 
-    if (tx.category === '❓Misc') tx.category = '❓ Miscellaneous';
+    if (tx.category === "❓Misc") tx.category = "❓ Miscellaneous"
 
-    tx.id = generateId ();
+    tx.id = generateId()
 
     // delete extra keys from the transaction object
-    delete tx.for;
-    delete tx.by;
-    delete tx.converted_total;
-    delete tx.recurring;
+    delete tx.for
+    delete tx.by
+    delete tx.converted_total
+    delete tx.recurring
   }
 
-  var ids = [];
+  var ids = []
 
   for (var i = 0; i < txs.length; i += 500) {
-    var txChunk = txs.slice (i, i + 500);
-    var txChunkIds = await db ('transactions').insert (txChunk).returning ('id');
-    ids.push (...txChunkIds);
+    var txChunk = txs.slice(i, i + 500)
+    var txChunkIds = await db("transactions").insert(txChunk).returning("id")
+    ids.push(...txChunkIds)
   }
 
-  ids.forEach ((id, index) => txsMemberJunctionRows[index].forEach (row => row.transaction_id = id.id));
-  txsMemberJunctionRows = txsMemberJunctionRows.flat ();
+  ids.forEach((id, index) => txsMemberJunctionRows[index].forEach((row) => (row.transaction_id = id.id)))
+  txsMemberJunctionRows = txsMemberJunctionRows.flat()
 
   // insert rows into the transactions_members join table in 500 row chunks
   for (var i = 0; i < txsMemberJunctionRows.length; i += 500) {
-    await db ('transactions_member_junction').insert (txsMemberJunctionRows.slice (i, i + 500));
+    await db("transactions_member_junction").insert(txsMemberJunctionRows.slice(i, i + 500))
   }
-
 }
 
-await createTables ();
+await createTables()
 
-const filenames = process.argv.slice (2).map (filename => path.parse (filename));
+const filenames = process.argv.slice(2).map((filename) => path.parse(filename))
 
 for (var filename of filenames) {
-  console.log (`Importing ${filename.base} into ledger ${filename.name}`);
-  const fullpath = path.join (filename.dir, filename.base);
-  await importTransactionsFromJsonFile (fullpath, formatLedgerName(filename.name));
+  console.log(`Importing ${filename.base} into ledger ${filename.name}`)
+  const fullpath = path.join(filename.dir, filename.base)
+  await importTransactionsFromJsonFile(fullpath, formatLedgerName(filename.name))
 }
 
-process.exit (0);
+process.exit(0)
